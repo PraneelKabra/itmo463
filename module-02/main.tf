1c6fd1cc69c0294d5edab2164bd3eb771a9c9a4f
Praneel Kabra <praneel.kabra@gmail.com>
2025-09-30 06:02:12 +0530

    typo

--- module-02/main.tf
+++ module-02/main.tf
@@ -194,7 +194,7 @@ resource "aws_lb_target_group" "alb-lb-tg" {
   target_type = "instance"
   port = 80
   protocol = "HTTP"
-  vpc_id = data.aws_vpc.main
+  vpc_id = data.aws_vpc.main.id
 }
   
 ##############################################################################
