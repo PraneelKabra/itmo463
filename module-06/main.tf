acef44b7fd8bfa7300bfad7547580ea2f989bec4
Praneel Kabra <praneel.kabra@gmail.com>
2025-10-07 06:26:52 +0530

    fix for unhealthy instances

--- module-06/main.tf
+++ module-06/main.tf
@@ -57,9 +57,9 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
   to_port           = 22
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_mysql" {
+resource "aws_vpc_security_group_ingress_rule" "allow_http_from_elb" {
   security_group_id = aws_security_group.allow_http.id
-  cidr_ipv4         = "0.0.0.0/0"
+  source_security_group_id = aws_security_group.allow_http.id
   from_port         = 3306
   ip_protocol       = "tcp"
   to_port           = 3306
